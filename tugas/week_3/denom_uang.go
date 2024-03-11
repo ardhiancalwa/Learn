3a7ef1d0cb722664e757bcf9f1c5725b8bdd8ab2
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-03-11 08:43:37 +0700

    update: tugas

--- tugas/week_3/denom_uang.go
+++ tugas/week_3/denom_uang.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
-func denom_uang(uang int) {
-	var lembar10, lembar2, lembar1 int
-	lembar10 = uang / 10000
+func denom_uang(uang int, k10, k2, k1 *int) {
+	*k10 = uang / 10000
 	uang = uang % 10000
-	lembar2 = uang / 2000
+	*k2 = uang / 2000
 	uang = uang % 2000
-	lembar1 = uang / 1000
-	fmt.Println(lembar10, "lembar 10000")
-	fmt.Println(lembar2, "lembar 2000")
-	fmt.Println(lembar1, "lembar 1000")
+	*k1 = uang / 1000
+	fmt.Println(*k10, "lembar 10000")
+	fmt.Println(*k2, "lembar 2000")
+	fmt.Println(*k1, "lembar 1000")
 }
 
 func main() {
-	var uang int
+	var uang, k10, k2, k1 int
 	fmt.Scan(&uang)
-	denom_uang(uang)
+	denom_uang(uang, &k10, &k2, &k1)
 }
