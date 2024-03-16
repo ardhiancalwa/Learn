7ce13585eb0b1ec156b2bec23a191cdcd055656a
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-03-16 14:15:41 +0700

    update coding practice

--- coding_practice/week_4/pola_genap.go
+++ coding_practice/week_4/pola_genap.go
@@ -10,11 +10,17 @@ func main() {
 	fmt.Print(result)
 }
 
-func printStars(N int, i int, j int, result string) string {
-	if i == N {
+func printStars(N, i, j int, result string)string {
+    if i > N {
 		return result
-	} else {
-		result += "*"
-		return printStars(N, i+1, j+1, result)
+	} 
+
+	if i % 2 == 0 {
+		if j < i {
+			return printStars(N, i, j+1, result+"*")
+		} else {
+			return printStars(N, i+1, 0, result+"\n")
+		}
 	}
+	return printStars(N, i+1, j, result)
 }
